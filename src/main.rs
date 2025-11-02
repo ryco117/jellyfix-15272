d0d68fe4ef3d66ed90110805e150d1ce71e10418
Ryan Andersen <ryco117@gmail.com>
2025-11-02 11:48:46 -0800

    More nit-picking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 
-
 #[derive(Parser)]
 #[command(about = "Small tool to fix jellyfin issue 15272")]
 struct Args {
@@ -8,39 +7,48 @@ struct Args {
     database_path: String,
 }
 fn main() {
-    let Args {database_path} = Args::parse();
+    let Args { database_path } = Args::parse();
     println!("Fixing jellyfin issue 15272 for database at: {database_path}");
 
     // Use sqlite to open the database.
     let conn = sqlite::Connection::open(&database_path).expect("Failed to open database");
     println!("Database opened successfully.");
 
+    // Find all items with null `DateCreated` but non-null `AlbumArtists` (implying it is an album).
     let mut rows = Vec::new();
-    conn.iterate("select Id, Name from BaseItems where DateCreated is null and AlbumArtists is not null", |row| {
-        let id = row[0].1.expect("Null `Id` column is null");
-        let album_name = row[1].1.expect("Null `Name` column is null");
-        
-        println!("Found album {album_name}");
-        rows.push((id.to_string(), album_name.to_string()));
+    conn.iterate(
+        "select Id, Name from BaseItems where DateCreated is null and AlbumArtists is not null",
+        |row| {
+            let id = row[0].1.expect("Null `Id` column is invalid");
+            let album_name = row[1].1.expect("Null `Name` column is invalid");
 
-        true
-    }).expect("Failed to iterate over rows");
+            println!("Found album {album_name}");
+            rows.push((id.to_string(), album_name.to_string()));
+
+            true
+        },
+    )
+    .expect("Failed to iterate over rows");
 
     for (id, album_name) in rows {
-        // Use the `Id` column to get all items with this as the parent id.
+        // Use the album `Id` to get all items with this as the `ParentId`.
         let mut date_created = None;
-        let r = conn.iterate(format!("select DateCreated from BaseItems where ParentId = '{id}'"), |child_row| {
-            let child_date_created = child_row.get(0).map(|p| p.1).flatten();
-            if let Some(date) = child_date_created {
-                date_created = Some(date.to_owned());
-                false
-            } else {
-                true
-            }
-        });
+        let r = conn.iterate(
+            format!("select DateCreated from BaseItems where ParentId = '{id}'"),
+            |child_row| {
+                // Find the first item in the album with a non-null `DateCreated`.
+                let child_date_created = child_row.get(0).map(|p| p.1).flatten();
+                if let Some(date) = child_date_created {
+                    date_created = Some(date.to_owned());
+                    false
+                } else {
+                    true
+                }
+            },
+        );
 
         if let Err(e) = r {
-            // Ignore expected `abort` error when early exiting the date-search loop.
+            // Ignore expected `abort` error when early exiting the `DateCreated` search loop.
             if !matches!(e.code, Some(4)) {
                 eprintln!("Failed to iterate over child rows");
                 continue;
@@ -49,7 +57,10 @@ fn main() {
 
         // If we found a date created, update the parent item.
         if let Some(date) = date_created {
-            conn.execute(format!("update BaseItems set DateCreated = '{date}' where Id = '{id}'")).expect("Failed to update DateCreated");
+            conn.execute(format!(
+                "update BaseItems set DateCreated = '{date}' where Id = '{id}'"
+            ))
+            .expect("Failed to update DateCreated");
             println!("SUCCESS: Updated DateCreated for album: {album_name}");
         } else {
             eprintln!("FAILURE: No valid DateCreated found for album: {album_name}");
